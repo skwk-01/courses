22c6e4a0b32a077b3b802bea3bef5edc59ebc68d
skwk-01 <sksht.13@gmail.com>
2024-10-12 18:48:40 +0900

    Update settings.py

--- coursesapp/settings.py
+++ coursesapp/settings.py
@@ -124,6 +124,9 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
